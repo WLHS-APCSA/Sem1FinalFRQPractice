b38fc37c508c3cc180d9c4f6107055f4e9b4b952
Brandon Smith <88860661+smithbwlhs@users.noreply.github.com>
2025-01-17 23:30:33 -0800

    Create Clock.java

---
+++ Clock/Clock.java
@@ -0,0 +1,17 @@
+public class Clock {
+    // instance variables
+
+
+    // constructors
+
+
+    // accessors
+
+    // mutators
+    
+
+    @Override
+    public String toString{
+        return String.format("The time is %02d:%02d",hours,minutes);
+    }
+}
