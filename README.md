a8fefab4008acdc4b5167c631c313b3d02a2c757
smithbwlhs <smithb@wlwv.k12.or.us>
2025-01-18 13:47:59 -0800

    removed solution from Clock.java

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Semester 1 Final FRQ Practice
 
 * You will need to create a class and implement accessor and mutator methods. The mutator method will not just be a basic update value method and will possibly require topics we have learned in semester one.
+* If you clone this entire repo, you can run test to see if your code passes by right-clicking on src->test->java and Run All Test OR you can go into ClockTest.java and right click and run on each method run that test
 * Examples
   * Quadratic Equation Information
   * Clock Class  
--- src/main/java/Clock/Clock.java
+++ src/main/java/Clock/Clock.java
@@ -2,61 +2,12 @@ package Clock;
 
 public class Clock {
     // instance variables
-    int hours;
-    int minutes;
-
 
     // constructors
 
-    public Clock() {
-        hours = 12;
-        minutes = 0;
-    }
-
-    public Clock(int hours, int minutes){
-        this.minutes = minutes;
-        this.hours = hours;
-    }
-
-
     // accessors
-    public int getHours() {
-        return hours;
-    }
 
-    public int getMinutes() {
-        return minutes;
-    }
     // mutators
-
-
-    public void setHours(int hours) {
-        this.hours = hours;
-    }
-
-    public void setMinutes(int minutes) {
-        this.minutes = minutes;
-    }
-
-    public void setTime(int hours, int minutes){
-        this.hours = hours;
-        this.minutes = minutes;
-    }
-
-    public void next(){
-        if(minutes == 59 && hours != 11) {
-            minutes = 0;
-            hours = (hours+1)%12;
-        }
-        else if(minutes == 59){
-            minutes = 0;
-            hours = 12;
-        }
-        else{
-            minutes++;
-        }
-
-    }
 
     @Override
     public String toString(){
